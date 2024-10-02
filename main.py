db48beda28109cf3a5ffd3587ba0d0b238a5d344
Pablo Rigueiro <pablorigueiro@rai.usc.es>
2024-10-02 16:20:12 +0200

    Corrección argumentos triangulo.get_perimetro(argumentos)

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ lado = 4
 print(f"El área de un {cuadrado.get_identificador()} de lado {lado} es {cuadrado.get_area(lado)} y el perímetro es {cuadrado.get_perimetro(lado)}")
 base_triangulo = 4
 altura_triangulo = 2
+lado_a=4
+lado_b=8
+lado_c=8
 print(triangulo.get_identificador())
-print(f"El área de un {triangulo.get_identificador()} de base {base_triangulo} y altura {altura_triangulo} es {triangulo.get_area(base_triangulo, altura_triangulo)} y el perímetro es {triangulo.get_perimetro(base_triangulo, altura_triangulo)}")
+print(f"El área de un {triangulo.get_identificador()} de base {base_triangulo} y altura {altura_triangulo} es {triangulo.get_area(base_triangulo, altura_triangulo)} y el perímetro es {triangulo.get_perimetro(lado_a, lado_b, lado_c)}")
 base_rectangulo = 4
 altura_rectangulo = 2
 print(rectangulo.get_identificador())
